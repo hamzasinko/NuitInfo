303653bd2d43c40961a8ac07c9db947c05304811
YoussefAmegouz <youssef.amegouz@usmba.ac.ma>
2024-12-06 01:17:09 +0100

    youssef1

---
+++ src/components/AppTemplate.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react';
+
+function AppTemplate({children}) {
+  const backgroundVid = '/backgroundVid.mp4';
+  return (
+    <Box position="relative" padding="0" margin="0" height="100vh" width="100%" overflow="hidden">
+        <Box
+          as="video"
+          position="absolute"
+          top="0"
+          left="0"
+          width="100%"
+          height="100%"
+          objectFit="cover"
+          autoPlay
+          loop
+          muted // Ensures the video is muted
+        >
+          <source src={backgroundVid} type="video/mp4" />
+          Your browser does not support the video tag.
+        </Box>
+
+        <Box position="relative" zIndex="1" color="black" padding="20px">
+            {children}
+        </Box>
+    </Box>
+  )
+}
+
+export default AppTemplate
