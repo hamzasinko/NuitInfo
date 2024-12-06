7fee8c33c64d64aa40d1113e6a03f24516744f80
YoussefAmegouz <youssef.amegouz@usmba.ac.ma>
2024-12-06 01:57:00 +0100

    v3

--- src/components/AppTemplate.js
+++ src/components/AppTemplate.js
@@ -1,31 +1,36 @@
 import React from 'react'
 import { Box } from '@chakra-ui/react';
+import Footer from './Footer';
 
 function AppTemplate({children}) {
   const backgroundVid = '/backgroundVid.mp4';
-  return (
+return (
+    <>
     <Box position="relative" padding="0" margin="0" height="100vh" width="100%" overflow="hidden">
-        <Box
-          as="video"
-          position="absolute"
-          top="0"
-          left="0"
-          width="100%"
-          height="100%"
-          objectFit="cover"
-          autoPlay
-          loop
-          muted // Ensures the video is muted
-        >
-          <source src={backgroundVid} type="video/mp4" />
-          Your browser does not support the video tag.
-        </Box>
-
-        <Box position="relative" zIndex="1" color="black" padding="20px">
-            {children}
-        </Box>
+            <Box
+                as="video"
+                position="absolute"
+                top="0"
+                left="0"
+                width="100%"
+                height="100%"
+                objectFit="cover"
+                autoPlay
+                loop
+                muted // Ensures the video is muted
+            >
+                <source src={backgroundVid} type="video/mp4" />
+                Your browser does not support the video tag.
+            </Box>
+            <Box position="relative" zIndex="1" color="black" padding="20px">
+                    {children}
+            </Box>
     </Box>
-  )
+    <Box position="absolute" m={0} bottom="0" width="100%" zIndex="1" color="white" textAlign="center">
+        <Footer/>
+    </Box>
+    </>
+)
 }
 
 export default AppTemplate
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box, Text, Link, List, ListItem } from '@chakra-ui/react';
+
+const Footer = () => {
+    return (
+        <Box as="footer" p={4} bg="gray.800" color="white">
+            <Box textAlign="center">
+                <Text>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</Text>
+                <List styleType="none" display="flex" justifyContent="center" mt={2}>
+                    <ListItem mx={2}>
+                        <Link href="/about" color="teal.200">About Us</Link>
+                    </ListItem>
+                    <ListItem mx={2}>
+                        <Link href="/contact" color="teal.200">Contact</Link>
+                    </ListItem>
+                    <ListItem mx={2}>
+                        <Link href="/privacy" color="teal.200">Privacy Policy</Link>
+                    </ListItem>
+                </List>
+            </Box>
+        </Box>
+    );
+};
+
+export default Footer;
